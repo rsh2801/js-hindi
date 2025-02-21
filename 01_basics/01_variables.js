fe40985a4ff364fc602d6f1ca5614feefe1e2bfc
rsh2801 <rsh280104@gmail.com>
2025-02-21 16:46:45 +0000

    basics variables dec

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountid= 144553;
+let accountemail= "rs2022jee@gmail.com"
+var accountpassword= "12345"
+accountcity= "jamshedpur"
+let accountstate
+
+// accoundid = 2; const variable is const changs not allowed
+
+
+console.log(accountid);
+
+// var let - both are use for variavble dicleration
+// but donot use var as no concept of scope is applied with var
+
+console.table([accountid,accountemail,accountpassword,accountcity]);
+accountemail="gfg"
+accountpassword="123"
+accountcity= "ranchi"
+console.table([accountid,accountstate,accountemail,accountpassword,accountcity]);
+
