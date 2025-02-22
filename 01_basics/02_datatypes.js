02c0d992f94d7735629f99ac5c5fd5bf9e6fde19
rsh2801 <rsh280104@gmail.com>
2025-02-22 18:14:02 +0000

    join it

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict" //treat all js code as newer verser
+//mahi bhi likha jae to bhi newer he leha
+
+console.log(2+3); console.log(3+6);
+//semicolon strict nahi hai next line me likh do, same line me use kar rahe ho to use kar loo
+
+//mdm and ecma is for documentation
+
