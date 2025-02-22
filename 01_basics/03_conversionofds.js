42e00744f52e3e67c7077f2f68011941e54c972c
rsh2801 <rsh280104@gmail.com>
2025-02-22 22:11:32 +0000

    extra

---
+++ 01_basics/03_conversionofds.js
@@ -0,0 +1,29 @@
+let score = "hitesh"
+// datatype are written in small alpha and conversion to data me 1st lett cap as Number
+ 
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
