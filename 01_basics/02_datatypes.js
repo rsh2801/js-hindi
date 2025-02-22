18657152f34feb3e76da0f91a6cdaf81ce7a5e42
rsh2801 <rsh280104@gmail.com>
2025-02-22 18:23:00 +0000

    types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,24 @@ console.log(2+3); console.log(3+6);
 
 //mdm and ecma is for documentation
 
+let age = 18
+let isloged= false
+let state = null
+
+// number ==2^
+// bigint 
+//string = ""
+//boolean
+//null = standalone value showing nothing diff from undefined
+//undefined = not defined yet
+//symbol = unique thing
+
+
+//object also
+
+
+console.log(typeof null); //give type of variable
+console.log(typeof undefined);
+
+  //note ====null is a object
+//
