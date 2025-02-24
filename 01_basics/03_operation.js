c7280cf549701cda437fe917d93bb30d3c78b155
rsh2801 <rsh280104@gmail.com>
2025-02-24 11:47:59 +0000

    conversion comp

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,45 @@
+let val=3;
+let negval= -val;
+
+console.log(negval);
+
+console.log(2+3);
+
+let s1="roshan"
+let s2="singh"
+console.log(s1+s2);//string contacination
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+
+console.log("1"+2+2); //string first me tha so string consider kiya 
+console.log(1+2+"2");//frst me number tha to number jaisa chala fir string tha too string 
+
+console.log(true);
+console.log(+true);//1 print
+
+// console.log(true+);//not possible error
+
+let num1, num2 ,num3
+num1=num2=num3=2+2  //allowed
+
+console.log(num1);
+
+
+
+let gamecounter=100;
+console.log(gamecounter++) //precedence operator
+console.log(++gamecounter)
+
+
+
+
+
+
+
+
+
+
+
+
