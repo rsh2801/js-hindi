f211996dfdeee1abfc8398c2cbd4f0b492da9ae1
rsh2801 <rsh280104@gmail.com>
2025-02-24 16:08:55 +0000

    changes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// comparing string with nos 
+console.log("2" >1);  //auto matically converts string 2 to nos.
+console.log("02">1);
+
+console.log(null > 0);  //both == and conversion >=, <= work diff
+console.log(null == 0);//comparisions >,<,>= etc convert null to zero so 0> 0 false
+console.log(null >=0); //while == not convert to 0 it convert to NAN so null == 0 is false
+
+
+
+
+
