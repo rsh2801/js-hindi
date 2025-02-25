a8aa124edf44f86215f3ae23d9a51dc038eca270
rsh2801 <rsh280104@gmail.com>
2025-02-25 22:44:35 +0000

    EST

--- 01_basics/06_string_in.js
+++ 01_basics/06_string_in.js
@@ -20,6 +20,38 @@ what you had to do and began,`;
 console.log(newline);
 
 
+//e.coercion a string eg null->"null", true->"true"
+// Depending on your use case, you may want to use `${x}` (to mimic built-in behavior) or 
+// String(x) (to handle symbol values without throwing an error), but you should not use "" + x
+
+
+//e.  String with new keyword returns a string wrapper object with key value pair see in console.
+const gameName = new String("l-u-d-o"); 
+console.log(typeof gameName); 
+
+console.log(gameName.__proto__)
+console.log(gameName.length);
+console.log(gameName.toUpperCase());//do in stack orginal gameName string remain same
+console.log(gameName);
+console.log(gameName.charAt(2));//kon sa char hai given pos pee
+console.log(gameName.indexOf(2));//char kon se index pe hai
+
+//substring 
+const newstr=gameName.substring(0,4);//start pos to end diya jata hai hai end ind leta nahi hai
+console.log(newstr);
+
+//slice
+const newsli=gameName.slice(-6,4) //ye bhi substr ke liye hota hai but neg ind de ke isme ghum ke peeche se le sakta hai
+//substr me neg value dega to 0 le lega usko
+console.log(newsli);
+
+
+
+
+
+
+
+
 
 
 
