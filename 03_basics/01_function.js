5714987977aaf0db5a11dafc37fcdf73111ef1c3
rsh2801 <rsh280104@gmail.com>
2025-03-25 18:27:13 +0000

    INITIAL

---
+++ 03_basics/01_function.js
@@ -0,0 +1,73 @@
+//-------------------------------------------------------------------------FUNCTION DECLEARATION,EXTRACTION,RETURN----------------------------------------------------------//
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+//calling****
+// sayMyName()
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(3, 5)
+// console.log("Result: ", result);
+
+
+
+//--------------------------------------------------------------------------ARGUMENT-PARAMETER-RETURN TYPE OF EMPTIED ARGUMENT FXN------------------------------------------------------------------------------//
+//FXN DEFINATION TIME INPUT -parameter
+//CALLING TIME FXN IMPUT- argument
+//FUNCTION KE SAMAY AGAR KUCH PASS NAHI KIYA TO undefined OUTPUT AATA HAI, WHI INT HAI STRING PAAS KAR DIYA TO STRING LETURE ME OPERATION PAR STRING JAISE BEHAVE KARTA HAI WAISA KAREGA
+
+
+function loginUserMessage(username){   
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
