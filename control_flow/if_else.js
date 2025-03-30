6e08c49aea5ff4a82b0bedf8e61f44bd4e930cbe
Roshan Singh <181599819+rsh2801@users.noreply.github.com>
2025-03-30 12:57:27 +0000

    if_else

---
+++ control_flow/if_else.js
@@ -0,0 +1,36 @@
+
+// <, >, <=, >=, ==, !=, ===, !==
+// -------------------------------------------------------------------------simple ex------------------------------------------------------------------------------//
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
+
+//---------------------------------------------------------------------------------------writing in one line not readable but jaan loo--------------------------------------------------------------//
+//comma and semicolon necessar here
+
+const balance = 1000
+
+if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");
+
+
+
