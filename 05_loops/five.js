3fd34e727a544d58e2bd0a4069afbc01e5b408a7
Roshan Singh <181599819+rsh2801@users.noreply.github.com>
2025-03-31 13:13:28 +0000

    for each loop arr.firEach(fxn(){})

--- 05_loops/five.js
+++ 05_loops/five.js
@@ -1,23 +1,37 @@
+//--------------------------------------------------------------------------for each loop-----------------------------------------------------------//
+//syntax array_name.forEach( function(parameters){
+//kya karna hai array ke parameters ka});
+
+
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// coding.forEach( function (val){
+//**********forEach ke aandar eek call back fxn hota hai i.e fxn without a name***********/
+
+//normal fxn*******
+coding.forEach( function (val){
 //     console.log(val);
-// } )
+} )
 
-// coding.forEach( (item) => {
+//arrow fxn******
+coding.forEach( (item) => {
 //     console.log(item);
-// } )
+} )
 
-// function printMe(item){
-//     console.log(item);
-// }
+//fxn with refrence only not call i.e. - only(printMe) not (printMe())******
+function printMe(item){
+    console.log(item);
+}
 
 // coding.forEach(printMe)
 
-// coding.forEach( (item, index, arr)=> {
-//     console.log(item, index, arr);
-// } )
 
+//multiple argument******************************
+coding.forEach( (val,index,arr)=>{
+    // console.log(val,index,arr);
+    
+})
+
+//array of obj
 const myCoding = [
     {
         languageName: "javascript",
