12baaa1635e00e68bf62eae76d9a71eddf15c879
Roshan Singh <181599819+rsh2801@users.noreply.github.com>
2025-04-24 15:00:27 +0000

    done

--- 08_promises/01.js
+++ 08_promises/01.js
@@ -1,16 +1,34 @@
-function setTimeoutPromisified(ms){
-    let p= new Promise(resolve=>setTimeout(resolve , ms));
-    return p;
+//-------------------------------------------------------------------------promised created------------------------------------------------------------------------------//
+//a promise is better way to say completion of a task
+const promiseOne = new Promise(function(resolve, reject){    //resolve - reject is also a fxn
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(resolve,3000);                             //jab resolve call hoga to .then() me promise consume hoga
+})
+
+
+//***using the promising class */
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+//--------------------------------------------------------------------readFile promise--------------------------------------------------------------------------------------------//
+const fs = require("fs");
+
+function clear(resolve) {
+    fs.readFile("a.txt", 'utf-8', function(err, data) {
+    
+        setTimeout(() => resolve(data), 3000);
+    });
 }
 
-function callback(){
-    console.log("3 seconds passed");
-}
+const clear_File = new Promise(clear);
 
-//****same as setTimeout(callback,3000);
-// setTimeoutPromisified(3000).then(callback);
-
-
-//****this is returning instance of instance of promise class */
-let p = setTimeoutPromisified(3000)
-console.log(p);
+clear_File.then((data) => {
+    console.log(data);
+});
+//------------------------------------------------------------------------------understanding---------------------------------------------------------------------------//
+//1. promise eek class hai so new promise(---)  ----> iske aandar same jai new rect(10,20) daalte hai waise he eek fxn descibing about its asyn job bhejna hota hai
+//2. promise ke aandar eek fxn hota hai jisska argument do fxn hote as resolve() and reject()
+//3. after async task call resolve(data) to join it with .then(callback) {callback get data}
+//4.similarly for reject   to join with .catch();
---
+++ 08_promises/02.js
@@ -0,0 +1,55 @@
+
+
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+//     }, 1000)
+
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// })
+
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "Chai", email: "chai@example.com"})
+//     }, 1000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
+
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if (!error) {
+//             resolve({username: "hitesh", password: "123"})
+//         } else {
+//             reject('ERROR: Something went wrong')
+//         }
+//     }, 1000)
+// })
+
+//  promiseFour
+//  .then((user) => {
+//     console.log(user);
+//     return user.username
+// }).then((username) => {
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
+// const promiseFive = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if (!error) {
+//             resolve({username: "javascript", password: "123"})
+//         } else {
+//             reject('ERROR: JS went wrong')
+//         }
+//     }, 1000)
+// });
